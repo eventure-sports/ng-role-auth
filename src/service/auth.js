66735872d510109779fea795ef97f0b779bbce35
Sergey Kuznetsov <sergey.kuznetsov@hotmail.com>
2015-03-13 18:04:22 +0300

    async version

--- src/service/auth.js
+++ src/service/auth.js
@@ -1,56 +1,64 @@
 (function(){
 	
-	function Service($rootScope, $route, $location, $window, NRA_MSG){
+	function Service($rootScope, $route, $location, $window, $q, NRA_MSG){
 		
 		var self = this;
 		
-		this.getRole = function(){
-			return null;
-		};
+		this.getRole = function () {
+            var deferred = $q.defer();
+            deferred.resolve(null);
+            return deferred.promise;
+        };
 		
-		this.isAuthorized = function(auth){
-			if(typeof auth === "string"){
-				auth = [auth];
-			}
-			
-			var roles = self.getRole();
-			if(typeof roles === "string"){
-				roles = [roles];
-			}
-			
-			var isAllowed = true;
-			if(auth[0]){
-				isAllowed = false;
-				for(var i = 0; i < roles.length; i++){
-					if(auth.indexOf(roles[i]) !== -1){
-						isAllowed = true;
-						break;
-					}
-				}
-			}
-			
-			return isAllowed;
-		};
+		this.isAuthorized = function (auth) {
+            var deferred = $q.defer();
+            if (typeof auth === "string") {
+                auth = [auth];
+            }
+
+            self.getRole().then(function (roles) {
+                if (typeof roles === "string") {
+                    roles = [roles];
+                }
+
+                var isAllowed = true;
+                if (auth[0]) {
+                    isAllowed = false;
+                    for (var i = 0; i < roles.length; i++) {
+                        if (auth.indexOf(roles[i]) !== -1) {
+                            isAllowed = true;
+                            break;
+                        }
+                    }
+                }
+                deferred.resolve(isAllowed);
+            });
+            return deferred.promise;
+        };
 		
 		this.onChange = function(event, next, prev){
 			var auth = next.$$route && next.$$route.authorized ? next.$$route.authorized : [];
 			
-			if(!self.isAuthorized(auth)){
-				$rootScope.$broadcast(NRA_MSG.accessDenied, (next.$$route ? next.$$route.originalPath : ""));
-				console.error("Access denied on unauthorized root:", (next.$$route ? next.$$route.originalPath : ""));
-				console.error("Reloading...");
-				event.preventDefault();
-				
-				if(!prev){
-					var url = "/";
-					if($route.routes[null]){
-						url = $route.routes[null].redirectTo;
-					}
-					$location.path(url).replace();
-				}
-			}
+			self.isAuthorized(auth).then(function (isAllowed) {
+                if (!isAllowed) {
+                    $rootScope.$broadcast(NRA_MSG.accessDenied, (next.$$route ? next.$$route.originalPath : ""));
+                    console.error("Access denied on unauthorized root:", (next.$$route ? next.$$route.originalPath : ""));
+                    console.error("Reloading...");
+
+                    if (prev && prev.$$route) {
+                        $location.path(prev.$$route.originalPath).replace();
+                    } else {
+                        var url = "/";
+                        if ($route.routes[null]) {
+                            url = $route.routes[null].redirectTo;
+                        }
+                        $location.path(url).replace();
+                    }
+
+                }
+            });
 		};
 	}
 	
-	angular.module("ngRoleAuth").service("AuthService", ["$rootScope", "$route", "$location", "$window", "NRA_MSG", Service]);
+	angular.module("ngRoleAuth").service("AuthService", ["$rootScope", "$route", "$location", "$window", "$q", "NRA_MSG", Service]);
 })();
